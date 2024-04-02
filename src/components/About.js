d21669a88408620c626a2996fa6b465d020ecc06
Wlodzimierz Niedzielski <biuro@rawio.pl>
2024-04-02 21:29:22 +0200

    test3

--- src/components/About.js
+++ src/components/About.js
@@ -22,7 +22,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#dfgdfg" className="btn">
+          <a href="#dfgdfgg" className="btn">
             read more
           </a>
         </article>
