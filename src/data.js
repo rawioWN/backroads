69ec4e5dd55e6dac9ffca553035d67af347c88e4
Wlodzimierz Niedzielski <biuro@rawio.pl>
2024-04-02 21:30:13 +0200

    fghfgh

--- src/data.js
+++ src/data.js
@@ -34,7 +34,7 @@ export const socialLinks = [
 export const services = [
   {
     id: 1,
-    title: "saving money",
+    title: "save money2",
     icon: "fas fa-wallet fa-fw",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
   },
